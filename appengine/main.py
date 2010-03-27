3f98aca642a3f230fee590d810e14b73c6f3946a
Pedro Valente <pedro.valente@gmail.com>
2010-03-27 00:53:05 -0300

    right file

--- appengine/main.py
+++ appengine/main.py
@@ -1,1 +1,74 @@
-../../frompdf/main.py
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from pyPdf.pdf import PdfFileReader
+import urllib2
+import StringIO
+
+
+
+
+class MainHandler(webapp.RequestHandler):
+
+  def get(self):
+    
+    url = self.request.get('url')
+    if not url:
+        url = ""
+    
+    
+    page = self.request.get('pg')
+    if not page:
+        page = 'all'
+    else:
+        page = int(page)-1
+    
+    u = urllib2.urlopen(urllib2.unquote(url))
+    
+    output = StringIO.StringIO()
+    output.write(u.read())
+    
+    p = PdfFileReader(output)
+
+    pages = p.getNumPages()
+    title = p.getDocumentInfo().title
+    if page == "all":
+        content=""
+        for i in range(0, pages):
+            # Extract text from page and add to content
+            content += "<page number='%d'><![CDATA[%s]]></page>\n" % (i+1,  p.getPage(i).extractText())
+    else:
+        content = "<page number='%d'><![CDATA[%s]]></page>\n" % (page+1,  p.getPage(page).extractText())
+    output.close()
+    
+    result = "<?xml version='1.0' encoding='UTF-8'?>\n<document url='%s' title='%s'>%s</document>" % (url, title, content)
+    
+    self.response.headers['Content-type'] = 'application/xml'
+    self.response.out.write(result)
+
+
+def main():
+  application = webapp.WSGIApplication([('/', MainHandler)],
+                                       debug=True)
+  util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+  main()
